e3ed11cd49948d35d90eb2efe60525e356a5975a
psych0wh1z <ikeagudike@gmail.com>
2023-09-12 22:08:28 +0100

    Define API routes and views

---
+++ api/v2/app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env pytuhon3
+"""HNGx Stage 2 API module"""
+
+from flask import Flask, jsonify
+
+from api.v2.views import app_views
+from models.person import db, Person
+
+
+app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.register_blueprint(app_views)
+db.init_app(app)
+
+
+with app.app_context():
+    db.create_all()
+
+
+@app.errorhandler(400)
+def not_found(error) -> str:
+    """Handle 400 errors"""
+    return jsonify({"error": error.description}), 400
+
+
+@app.errorhandler(404)
+def not_found(error) -> str:
+    """Handle 404 errors"""
+    return jsonify({"error": "Not found"}), 404
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ api/v2/views/persons.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+"""Module of User views
+"""
+from api.v2.views import app_views
+from flask import abort, jsonify, make_response, request
+from models.person import db, Person
+
+
+@app_views.route('/', methods=['GET'], strict_slashes=False)
+def view_persons() -> str:
+    """ GET /api
+    Return:
+      - List of all Person objects JSON represented
+    """
+    person_list = db.session.execute(
+                 db.select(Person).order_by(Person.id)).scalars()
+    persons = [person for person in person_list]
+    return make_response(jsonify(persons), 200)
+
+
+@app_views.route('/<int:user_id>', methods=['GET'], strict_slashes=False)
+def view_person_by_id(user_id: int) -> str:
+    """ GET /api/user_id
+    Path parameter:
+      - Person ID
+    Return:
+      - Person object JSON represented
+      - 400 if bad request format
+      - 404 if Person ID does not exist
+    """
+    if not Person.validate_id(user_id):
+        abort(400, description="User ID must be a positive integer > 0")
+    person = db.get_or_404(Person, user_id, description="User not found")
+    return make_response(jsonify(person), 200)
+
+
+@app_views.route('/', methods=['POST'], strict_slashes=False)
+def create_person() -> str:
+    """ POST /api
+    JSON body:
+      - name
+    Return:
+      - Person object JSON represented
+      - 400 if unable to create Person
+    """
+    if not 'name' in request.form:
+        abort(400, description="Missing name")
+    person = Person(name=request.form.get('name'))
+    db.session.add(person)
+    db.session.commit()
+    return make_response(jsonify(person), 201)
+
+
+@app_views.route('/<int:user_id>', methods=['PUT'], strict_slashes=False)
+def update_person(user_id: int) -> str:
+    """ PUT /api/user_id
+    Path parameter:
+      - Person ID
+    JSON body:
+      - name
+    Return:
+      - User object JSON represented
+      - 400 if unable to update Person
+      - 404 if Person ID does not exist
+    """
+    if not Person.validate_id(user_id):
+        abort(400, description="User ID must be a positive integer > 0")
+    if not 'name' in request.form:
+        abort(400, description="Missing name")
+    person = db.get_or_404(Person, user_id, description="User not found")
+    person.name = request.form.get('name')
+    db.session.commit()
+    return make_response(jsonify(person), 200)
+
+
+@app_views.route('/<int:user_id>', methods=['DELETE'], strict_slashes=False)
+def delete_person(user_id: int) -> str:
+    """ DELETE /api/user_id
+    Path parameter:
+      - Person ID
+    Return:
+      - Empty JSON if Person object has been correctly deleted
+      - 400 if unable to delete Person
+      - 404 if Person ID does not exist
+    """
+    if not Person.validate_id(user_id):
+        abort(400, description="User ID must be a positive integer > 0")
+    person = db.get_or_404(Person, user_id, description="User not found")
+    db.session.delete(person)
+    db.session.commit()
+    return make_response(jsonify({}), 204)
